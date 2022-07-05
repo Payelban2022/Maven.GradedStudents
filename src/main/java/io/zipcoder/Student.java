91e3d6602b2d209c85fcaedf7e5f3909bcaf31f0
payel <payel.nandy2580@gmail.com>
2022-07-04 22:52:19 -0400

    Added student method and parameter

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,56 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+
+
+
+
+    String firstName;
+    String lastName;
+
+    Double[] examScores;
+
+
+    public Student(String firstName, String lastName, Double[] examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNumberOfExamsTaken(){
+        return examScores.length;
+    }
+
+
+    public String getExamScores() {
+        String examScoresdata="";
+        int len=examScores.length;
+        for(int i=0; i<len;i++){
+            examScoresdata+="Exam "+(i+1)+" -> "+examScores[i].toString()+"\n";
+        }
+
+        return examScoresdata;
+    }
+
+    public void setExamScores(Double[] examScores) {
+        this.examScores = examScores;
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,65 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class StudentTest {
+    private String expectedFirstname;
+    private String expectedLastname;
+    private Double[] expectedexamScores;
+    Student student;
+
+    @Before
+    public void setUp(){
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores={ 100.0, 95.0, 123.0, 96.0 };
+        student = new Student(firstName, lastName, examScores);
+
+        this.expectedFirstname=firstName;
+        this.expectedLastname=lastName;
+        this.expectedexamScores=examScores;
+
+    }
+
+    @Test
+    public void testGetFirstName() {
+
+        String actualFirstName = student.getFirstName();
+        System.out.println(expectedFirstname+actualFirstName);
+        Assert.assertEquals(expectedFirstname,actualFirstName);
+
+
+
+    }
+
+
+
+   @Test
+    public void testGetLastName() {
+       String actualLastName = student.getLastName();
+       System.out.println(expectedLastname+actualLastName);
+       Assert.assertEquals(expectedLastname,actualLastName);
+    }
+
+
+@Test
+    public void testGetNumberOfExamsTaken() {
+        int expectedExamCount=4;
+        int actualExamCount=student.getNumberOfExamsTaken();
+    System.out.println(expectedExamCount+" "+actualExamCount);
+        Assert.assertEquals(expectedExamCount,actualExamCount);
+    }
+    @Test
+    public void testGetexamScore(){
+        String output = student.getExamScores();
+        System.out.println(output);
+
+    }
+
 
 }
